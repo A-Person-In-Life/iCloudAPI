ba973f13756611d766c4034897232576acb532ce
Gavin Weiner <gavin@Weiner-family.org>
2025-10-03 18:27:54 -0400

    2fa

--- iCloudAPI.py
+++ iCloudAPI.py
@@ -1,14 +1,30 @@
 from pyicloud import PyiCloudService
 from shutil import copyfileobj
 import os
+import sys
+
 
 with open("password.txt","r") as f:
 
     password = str(f.readline)
+    print(password)
     api = PyiCloudService("gavin.d.weiner@icloud.com",password)
 
     if api.requires_2fa:
-        print("needs 2fa")
+        print("Two-factor authentication required.")
+        code = input("Enter the code you received of one of your approved devices: ")
+        result = api.validate_2fa_code(code)
+        print("Code validation result: %s" % result)
+
+        if not result:
+            print("Failed to verify security code")
+            sys.exit(1)
+
+        if not api.is_trusted_session:
+            print("Session is not trusted. Requesting trust...")
+            result = api.trust_session()
+            print("Session trust result %s" % result)
+
 
 
 def push(folder_name):
@@ -26,8 +42,6 @@ def push(folder_name):
 
 def pull(folder_name):
     folder = api.drive[folder_name]
-
-
     pass
 
 push("Test_iCloudAPI")
