646afc6210b91767f8bd28f7f78f707476344d8c
Gavin Weiner <gavin@Weiner-family.org>
2025-10-04 09:55:47 -0400

    commit

--- iCloudAPI.py
+++ iCloudAPI.py
@@ -1,6 +1,8 @@
 from pyicloud import PyiCloudService
 import os
 import sys
+from shutil import copyfileobj
+import hashlib
 
 with open(r"C:\Users\gavin\OneDrive\Desktop\Python_Projects\password.txt","r") as f:
 
@@ -21,6 +23,17 @@ with open(r"C:\Users\gavin\OneDrive\Desktop\Python_Projects\password.txt","r") a
             print("Session is not trusted. Requesting trust...")
             result = api.trust_session()
             print(f"Session trust result {result}")
+
+
+def hash_file(file_path,hashing_algorithms="MD5"):
+    hash_function = hashlib.new(hashing_algorithms)
+
+    with open(file_path,"rb"):
+        with open(file_path, 'rb') as file:
+            while chunk := file.read(8192):
+                hash_function.update(chunk)
+    return hash_function.hexdigest()
+
 
 
 def clean_filename(filename):
@@ -59,6 +72,7 @@ def push(local_folder_path, icloud_folder_node=None):
         file_path = os.path.join(local_folder_path, entry)
 
         if os.path.isfile(file_path):
+            print(f"Entry {entry} is a file, preparing upload.")
             try:
                 icloud_file = icloud_folder_node[entry]
                 if os.stat(file_path).st_size == icloud_file.size:
@@ -75,7 +89,8 @@ def push(local_folder_path, icloud_folder_node=None):
                 icloud_folder_node.upload(f, filename=cleaned_name)
 
         elif os.path.isdir(file_path):
-            print(f"Recursing into subfolder: {entry}")
+
+            print(f"Entry {entry} is a subfolder, starting recursion into: {entry}")
             try:
                 subfolder_node = icloud_folder_node[entry]
                 print(f"Subfolder found: {entry}")
@@ -86,16 +101,33 @@ def push(local_folder_path, icloud_folder_node=None):
             push(file_path, subfolder_node)
 
 
-
-
-
-            
-                
-                
-
-
-def pull(local_folder_path,icloud_folder_name):
-
+def pull(local_folder_path, icloud_folder_name):
+    
     icloud_folder = api.drive[icloud_folder_name]
 
+    if icloud_folder is not None:
+        icloud_folder_contents = icloud_folder.dir()
+        if icloud_folder_contents is not None:
+            for entry in icloud_folder_contents:
+
+                try:
+                    
+                    local_folder_path[entry]
+                except KeyError:
+                    if os.path.isfile(entry):
+                        wi
+                        pass
+                    elif os.path.isdir(entry):
+                        pass
+                    else:
+                        print(f"Entry {entry} in iCloud folder {icloud_folder_name} is not a file like object")
+        else:
+            print(f"No entries found in iCloud folder '{icloud_folder_name}'.")
+    else:
+        print(f"iCloud Folder {icloud_folder_name} does not exist.")
+        return
+    
+
+
 push(r"C:\Users\gavin\Downloads\ASMR")
+pull(r"C:\Users\gavin\Downloads\ASMR","ASMR")
